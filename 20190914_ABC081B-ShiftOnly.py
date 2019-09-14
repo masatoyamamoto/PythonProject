8004601656ec4590770bb80e28540a7f0a93663e
Masato Max Yamamoto <masato.yamamoto10@gmail.com>
2019-09-14 13:24:27 +0900

    practice 3 and 4

--- 20190914_ABC081B-ShiftOnly.py
+++ 20190914_ABC081B-ShiftOnly.py
@@ -1,5 +1,11 @@
 # inputを受け取る
 numberOfNumbers = input()
-numbers = input().split()
+numbers = list(map(int, input().split()))
+# 回数のカウント
+a = 0
 
+while sum(list(map(lambda x: x % 2, numbers))) == 0:
+    numbers = list(map(lambda y: y / 2, numbers))
+    a += 1
 
+print(a)
