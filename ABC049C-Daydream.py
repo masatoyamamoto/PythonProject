44bc3da6be7785b72ae9f76f42063d1059826b1d
masatomaxyamamoto <masato.yamamoto10@gmail.com>
2020-01-26 16:22:09 +0900

    Traveling completed

--- ABC049C-Daydream.py
+++ ABC049C-Daydream.py
@@ -2,10 +2,10 @@
 S = input()
 T_candidates = ['dream', 'dreamer', 'erase', 'eraser']
 
-s = S.replace(T_candidates[3],"").replace(T_candidates[2],"").replace(T_candidates[1],"").replace(T_candidates[0],"")
+s = S.replace(T_candidates[3], "").replace(T_candidates[2], "").replace(T_candidates[1], "").replace(T_candidates[0],
+                                                                                                     "")
 
 if s:
     print("NO")
 else:
     print("YES")
-
--- ABC086C - Traveling.py
+++ ABC086C - Traveling.py
@@ -1,2 +1,9 @@
 # 経路探索
+n = int(input())
 
+for i in range(n):
+    t,x,y = map(int, input().split())
+    if (x+y) > t or (x + y + t) % 2:
+        print("No")
+        exit()
+print("Yes")
