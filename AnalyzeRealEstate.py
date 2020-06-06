847f20402fe8d3dad7696cbfffd4a657a10c6277
masatomaxyamamoto <masato.yamamoto10@gmail.com>
2020-06-06 16:57:13 +0900

    test

--- AnalyzeRealEstate.py
+++ AnalyzeRealEstate.py
@@ -4,4 +4,7 @@ import requests
 import pandas as pd
 from pandas import Series, DataFrame
 import time
+import slackweb as sw
 
+slack = sw.Slack(url="https://hooks.slack.com/services/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")
+slack.notify(text="処理が終わりました")
