339d3a7e98036799e9a337f3f87a7ae8aa419888
MasatoYamamoto <masato.yamamoto10@gmail.com>
2020-10-06 23:08:19 +0900

    Daydream completed

--- 20200607_AnalizeRealEstate.py
+++ 20200607_AnalizeRealEstate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # データ読み込み
 df = pd.read_csv("suumoData.csv", sep="\t", encoding="utf-16")
@@ -15,11 +16,35 @@ df_split_1["station"] = df_split_1["station"].str.replace("」(.)", "")
 df_split_1["time"] = df_split_1["time"].str.replace("分", "")
 # 切り分けたので元データを削除
 df.drop(['station'], axis=1, inplace=True)
+df = pd.concat([df, df_split_1], axis=1)
 
 # 広さのm2等を削除する
 df["size"] = df["size"].replace("m2(.*)", "", regex=True)
 df["terrace"] = df["terrace"].replace("m2(.*)", "", regex=True)
 
-# 値段を扱いやすくする。億円以上のものは億円部分を切り離して、*10,000して足したい(20200614)。
-df_split_1 = df["price"].str.split("億", expand=True)
+# 値段を扱いやすくする。
+df = df[~df["price"].str.contains("~")]
+df = df[~df["price"].str.contains("-")]
+df = df[~df["price"].str.contains("～")]
+df = df[~df["price"].str.contains("※")]
+df = df[~df["price"].str.contains("・")]
 
+# いっそ1億円以上は分析対象外(20200927）
+df = df[~df["price"].str.contains("億")]
+df["price"] = df["price"].replace("万円", "0000", regex=True)
+
+# 数値データの-を0に変換
+df["size"] = df["size"].replace("-", 0)
+df["terrace"] = df["terrace"].replace("-", 0)
+
+# 23区内に分析対象を限定
+df = df[~df["location"].str.contains("市")]
+df_split_1 = df["location"].str.split("区", expand=True)
+df_split_1.columns = ["ku", "cho-son"]
+df = pd.concat([df, df_split_1], axis=1)
+df.drop(["location"], axis=1, inplace=True)
+
+# 分析の記録
+df.to_csv("sumoAnalyze.csv", encoding="utf-16")
+
+# 本格的な分析
---
+++ 20201004_sumoAnalysis.R
@@ -0,0 +1,1 @@
+data = read.csv("sumoAnalyze.csv",  fileEncoding = "UTF-16")
