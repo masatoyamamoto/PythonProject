dd3d52181998497dddf4694c25869ea886db80c3
masatomaxyamamoto <masato.yamamoto10@gmail.com>
2021-01-26 00:45:42 +0900

    formatted price

--- 20200607_AnalizeRealEstate.py
+++ 20200607_AnalizeRealEstate.py
@@ -19,5 +19,12 @@ df.drop(['station'], axis=1, inplace=True)
 df["size"] = df["size"].replace("m2(.*)", "", regex=True)
 df["terrace"] = df["terrace"].replace("m2(.*)", "", regex=True)
 
+# 価格を使えるデータに整形する
+df = df[~df["price"].str.contains("~|〜|・|〜|～|※")]  # 注釈とか幅をもたせたデータは削除
+
+# 億円以上の部分
+df_split_1 = df["price"].str.extract('(.*億)')[0].str.replace("億","").fillna(0).astype(int)
+
+df.to_csv("test.csv")
 
 
